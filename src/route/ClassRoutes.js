1b6ff8666014ff64b910b48ca31470c69c971d6d
GuilhermeDeconto <deconto100@gmail.com>
2020-09-30 23:37:17 -0300

    Mocking class endpoints

--- src/route/ClassRoutes.js
+++ src/route/ClassRoutes.js
@@ -1,19 +1,19 @@
 "use strict"
 
-var ClassController = require('../controller/ClassController');
+var ClassController = require("../controller/ClassController");
 /** TODO: Create validation scheme for model. */
-//var classValidate = require('src/validate/class');
+//var classValidate = require("src/validate/class");
 
 module.exports = () => {
     return [
         {
-            method: 'GET',
-            path: '/index',
+            method: "GET",
+            path: "/index",
             options: {
-                auth: 'simple',
-                description: 'Index endpoint to test if application is up and running',
-                notes: 'Returns a hello world',
-                tags: ['api'],
+                auth: "simple",
+                description: "Index endpoint to test if application is up and running",
+                notes: "Returns a hello world",
+                tags: ["api"],
                 handler: async (request, resp) => {
                     try {
                         var data = {
@@ -28,17 +28,59 @@ module.exports = () => {
             }
         },
         {
-            method: 'GET',
-            path: '/class/{classId}',
-            config: {
-                handler: () => { }
+            method: "GET",
+            path: "/class/{classId}",
+            options: {
+                auth: "simple",
+                description: "Returns a class with given ID",
+                notes: "Returns class information with given ID",
+                tags: ["api"],
+                handler: async (request, resp) => {
+                    try {
+                        var data = {
+                            success: "true",
+                            message: "Hello world"
+                        }
+                        return resp.response(data);
+                    } catch (error) {
+                        return resp.response(error).code(500);
+                    }
+                }
             }
         },
         {
-            method: 'POST',
-            path: '/class',
-            config: {
-                handler: () => { }
+            method: "GET",
+            path: "/class",
+            options: {
+                auth: "simple",
+                description: "Returns all classes",
+                notes: "Returns all registered classes",
+                tags: ["api"],
+                handler: async (request, resp) => {
+                    try {
+                        //let classes = await ClassModel.find().exec();
+                        let classes = [
+                            {
+                                id: 1,
+                                name: "Programação distribuida",
+                                professor: "Sergio Johann"
+                            },
+                            {
+                                id: 1,
+                                name: "Construção de software",
+                                professor: "Eduardo Arruda"
+                            }
+                        ]
+                        var data = {
+                            success: "true",
+                            message: "Classes retrieved successfully",
+                            classes: classes
+                        }
+                        return resp.response(data);
+                    } catch (error) {
+                        return resp.response(error).code(500);
+                    }
+                }
             }
         }
     ];
