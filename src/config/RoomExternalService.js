e98bd10d8325ef3b0ed81e9ed0ed14651903bc4e
Joao Nascimento <joao.nascimento@sensedia.com>
2020-10-26 18:31:37 -0300

    Correct room endpoint

--- src/config/RoomExternalService.js
+++ src/config/RoomExternalService.js
@@ -2,6 +2,6 @@
 
 //TODO: change hostname, port and methods. Actual parameters = contents microservice.
 // this file should reflect rooms microservice.
-exports.HOSTNAME = '192.168.100.19'
-exports.PORT = '8080'
+exports.HOSTNAME = 'ec2-54-198-241-57.compute-1.amazonaws.com'
+exports.PORT = '3001'
 exports.GET = '/room/'
