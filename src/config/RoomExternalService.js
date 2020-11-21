3b691a94cf5d7711f71d8dd1ebb1a94344ef5736
Lotusdigital <gabriel.silva.010@acad.pucrs.br>
2020-11-21 15:56:16 -0300

    Room ec2 url updated

--- src/config/RoomExternalService.js
+++ src/config/RoomExternalService.js
@@ -1,5 +1,5 @@
 "use strict";
 
-exports.HOSTNAME = 'ec2-18-220-149-196.us-east-2.compute.amazonaws.com'
+exports.HOSTNAME = 'ec2-3-23-106-145.us-east-2.compute.amazonaws.com'
 exports.PORT = '3001'
 exports.GET = '/room/'
