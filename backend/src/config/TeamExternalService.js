e31e2c483720ca7abbb839aaa86d396233707573
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-12-01 17:38:28 -0300

    Fix team service

--- backend/src/config/TeamExternalService.js
+++ backend/src/config/TeamExternalService.js
@@ -1,5 +1,5 @@
 "use strict";
 
-exports.HOSTNAME = 'ec2-34-238-114-89.compute-1.amazonaws.com'
+exports.HOSTNAME = 'ec2-34-238-241-74.compute-1.amazonaws.com'
 exports.PORT = '3000'
 exports.GET = '/turma'
